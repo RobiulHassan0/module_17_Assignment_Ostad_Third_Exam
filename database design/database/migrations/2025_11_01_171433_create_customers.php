9c6b0e2cc7fdd62475f158be0a81c37c28797f0d
RobiulHassan0 <robiulhassan270@gmail.com>
2025-11-01 23:16:28 +0600

    tables created

--- database design/database/migrations/2025_11_01_171433_create_customers.php
+++ database design/database/migrations/2025_11_01_171433_create_customers.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->string('sku')->nullable()->after('name');
-            $table->decimal('weight', 10, 2)->after('price');
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -22,8 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('products', function (Blueprint $table) {
-            $table->dropColumn(['sku', 'weight']);
-        });
+        Schema::dropIfExists('customers');
     }
 };
--- database design/database/migrations/2025_11_01_171540_create_invoice_products.php
+++ database design/database/migrations/2025_11_01_171540_create_invoice_products.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::rename('categories', 'product_categories');
+        Schema::create('invoice_products', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::rename('product_categories', 'categories');
+        Schema::dropIfExists('invoice_products');
     }
 };
