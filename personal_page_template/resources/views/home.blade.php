b785947d88829f93a29c6927504bf1139bc832c9
RobiulHassan0 <robiulhassan270@gmail.com>
2025-11-05 23:56:27 +0600

    project almost done

---
+++ personal_page_template/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    @include('components.head-banner')
+
+    @include('components.about')
+@endsection
--- personal_page_template/routes/web.php
+++ personal_page_template/routes/web.php
@@ -1,26 +1,7 @@
 <?php
 
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/info', function (Request $request) {
+Route::get("/", [FrontendController::class, 'home']);
 
-//     $name = $request->query('name');
-//     $email = $request->query('email');
-
-//     $request->session()->put('name', $name);
-//     $request->session()->put('email', $email);
-
-//     Log::info('User Info:', ['name' => $name, 'email' => $email]);
-
-//     return response()->json([
-//         'status' => 'success',
-//         'message' => 'User info stored in session and logged.',
-//         'code' => 201
-//     ]);
-// });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
